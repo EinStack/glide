95ca553040eff76685bfa42fad5302ae6930d0c8
Max <mkrueger190@gmail.com>
2023-12-15 10:18:49 -0700

    #21: revert here for use of reflect

--- pkg/api/buildApiReq.go
+++ pkg/api/buildApiReq.go
@@ -22,18 +22,17 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-
 func Router(c *app.RequestContext) (interface{}, error) {
 	// this function takes the client request and returns the response from the provider
 	slog.Info("Router Function Called")
 
-    requestBody := c.Request.Body()
+	requestBody := c.Request.Body()
 
 	// Check if the request body is empty
-    if len(requestBody) == 0 {
+	if len(requestBody) == 0 {
 		slog.Error("request body cannot be empty")
-        return nil, errors.New("request body cannot be empty")
-    }
+		return nil, errors.New("request body cannot be empty")
+	}
 
 	// Send the request to the provider
 	resp, err := sendRequest(requestBody)
@@ -110,25 +109,25 @@ func definePayload(payload []byte, endpoint string) (providers.RequestDetails, e
 
 	slog.Debug("Payload: " + string(jsonString))
 
-	endpoints, ok := payload_data["endpoints"].([]interface{})
-	if !ok {
-		// Handle error
-		slog.Error("Endpoints not found")
+	endpointsInterface := payload_data["endpoints"].([]interface{})
+	var endpoints []map[string]interface{}
+
+	for _, endpoint := range endpointsInterface {
+		endpointMap, ok := endpoint.(map[string]interface{})
+		if !ok {
+			slog.Error("Invalid endpoint format")
+			continue
+		}
+		endpoints = append(endpoints, endpointMap)
 	}
 
 	jsonString, _ = json.Marshal(endpoints)
 
-	slog.Debug("Payload: " + string(jsonString))
+	slog.Info("Endpoint: " + string(jsonString))
 
 	providerList := make([]string, len(endpoints))
 	for i, endpoint := range endpoints {
-		endpointMap, ok := endpoint.(map[string]interface{})
-		if !ok {
-			// Handle error
-			fmt.Println("Endpoint is not a map")
-		}
-
-		provider, ok := endpointMap["provider"].(string)
+		provider, ok := endpoint["provider"].(string)
 		if !ok {
 			// Handle error
 			fmt.Println("Provider not found")
@@ -137,56 +136,65 @@ func definePayload(payload []byte, endpoint string) (providers.RequestDetails, e
 		providerList[i] = provider
 	}
 
+	slog.Info("Provider List: " + fmt.Sprintf("%v", providerList))
+
 	// TODO: Send the providerList to the provider pool to get the provider selection. Mode list can be used as well. Mode is the routing strategy.
 	//modeList := payload_data["mode"].([]interface{})
 
-	// TODO: the following is inefficient. Needs updating.
 
 	provider := "openai" // placeholder until provider pool is implemented
-
-	endpointsMap := payload_data["endpoints"].([]map[string]interface{})
 
 	var params map[string]interface{}
 
 	var api_key string
 
-	for _, endpoint := range endpointsMap {
+	for _, endpoint := range endpoints {
 		if endpoint["provider"] == provider {
-			params = endpoint["params"].(map[string]interface{})
-			api_key = endpoint["api_key"].(string)
-			fmt.Println(params)
+			params, _ = endpoint["params"].(map[string]interface{})
+			api_key, _ = endpoint["api_key"].(string)
 			break
 		}
 	}
 
-	var defaultConfig interface{}                   // Assuming defaultConfig is a struct
+	var defaultConfig interface{}                         // Assuming defaultConfig is a struct
 	var finalApiConfig providers.ProviderDefinedApiConfig // Assuming finalApiConfig is a struct
 
 	defaultConfig, finalApiConfig, _ = buildApiConfig(provider, api_key, endpoint)
 
+	slog.Info("Default Config: " + fmt.Sprintf("%v", defaultConfig))
+	slog.Info("Final API Config: " + fmt.Sprintf("%v", finalApiConfig))
+
 	// Use reflect to set the value in defaultConfig based on client payload
-	v := reflect.ValueOf(defaultConfig).Elem()
-	for key, value := range params {
-		field := v.FieldByName(key)
-		if field.IsValid() && field.CanSet() {
-			switch field.Kind() {
-			case reflect.Int:
-				if val, ok := value.(int); ok {
-					field.SetInt(int64(val))
-				}
-			case reflect.String:
-				if val, ok := value.(string); ok {
-					field.SetString(val)
-				}
-			}
-		}
-	}
+	v := reflect.ValueOf(defaultConfig)
+
+    for key, value := range params {
+        field := v.FieldByName(key)
+
+        if !field.IsValid() {
+            fmt.Printf("No such field: %s in defaultConfig", key)
+            continue
+        }
+
+        if !field.CanSet() {
+            fmt.Printf("Cannot set %s field value", key)
+            continue
+        }
+
+        fieldType := field.Type()
+        val := reflect.ValueOf(value)
+
+        if val.Type().ConvertibleTo(fieldType) {
+            field.Set(val.Convert(fieldType))
+        } else {
+            fmt.Printf("Provided value type didn't match defaultConfig field type")
+        }
+    }
 
 	// Validate the struct
 	validate := validator.New()
 	err = validate.Struct(defaultConfig)
 	if err != nil {
-		fmt.Printf("Validation failed: %v\n", err)
+		slog.Error("Validation failed: %v\n", err)
 		return providers.RequestDetails{}, err
 	}
 
@@ -203,6 +211,9 @@ func definePayload(payload []byte, endpoint string) (providers.RequestDetails, e
 }
 
 func buildApiConfig(provider string, api_key string, endpoint string) (interface{}, providers.ProviderDefinedApiConfig, error) {
+	
+	slog.Info("buildApiConfig Function Called")
+	
 	var defaultConfig interface{}
 	var apiConfig providers.ProviderApiConfig
 	var finalApiConfig providers.ProviderDefinedApiConfig
