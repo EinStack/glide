94b7840a4b4f9e396c72c1451b68a675254659c8
Roman Glushko <roman.glushko.m@gmail.com>
2023-12-31 18:23:13 +0200

    Linting

--- pkg/config/provider_test.go
+++ pkg/config/provider_test.go
@@ -3,19 +3,19 @@ package config
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestConfigProvider_NonExistingConfigFile(t *testing.T) {
 	_, err := NewProvider().Load("./testdata/doesntexist.yaml")
 
-	assert.Error(t, err)
-	assert.ErrorContains(t, err, "no such file or directory")
+	require.Error(t, err)
+	require.ErrorContains(t, err, "no such file or directory")
 }
 
 func TestConfigProvider_NonYAMLConfigFile(t *testing.T) {
 	_, err := NewProvider().Load("./testdata/provider.broken.yaml")
 
-	assert.Error(t, err)
-	assert.ErrorContains(t, err, "unable to parse config file")
+	require.Error(t, err)
+	require.ErrorContains(t, err, "unable to parse config file")
 }
