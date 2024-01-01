d73352943eab60ed738be570f197d311ed66c2f5
Max <mkrueger190@gmail.com>
2024-01-01 15:19:02 -0700

    #5: init

---
+++ pkg/providers/cohere/chat.go
@@ -0,0 +1,201 @@
+package cohere
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"glide/pkg/providers/errs"
+
+	"glide/pkg/api/schemas"
+	"go.uber.org/zap"
+)
+
+type ChatMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+// ChatRequest is an OpenAI-specific request schema
+type ChatRequest struct {
+	Model            string           `json:"model"`
+	Messages         []ChatMessage    `json:"messages"`
+	Temperature      float64          `json:"temperature,omitempty"`
+	TopP             float64          `json:"top_p,omitempty"`
+	MaxTokens        int              `json:"max_tokens,omitempty"`
+	N                int              `json:"n,omitempty"`
+	StopWords        []string         `json:"stop,omitempty"`
+	Stream           bool             `json:"stream,omitempty"`
+	FrequencyPenalty int              `json:"frequency_penalty,omitempty"`
+	PresencePenalty  int              `json:"presence_penalty,omitempty"`
+	LogitBias        *map[int]float64 `json:"logit_bias,omitempty"`
+	User             *string          `json:"user,omitempty"`
+	Seed             *int             `json:"seed,omitempty"`
+	Tools            []string         `json:"tools,omitempty"`
+	ToolChoice       interface{}      `json:"tool_choice,omitempty"`
+	ResponseFormat   interface{}      `json:"response_format,omitempty"`
+}
+
+// NewChatRequestFromConfig fills the struct from the config. Not using reflection because of performance penalty it gives
+func NewChatRequestFromConfig(cfg *Config) *ChatRequest {
+	return &ChatRequest{
+		Model:            cfg.Model,
+		Temperature:      cfg.DefaultParams.Temperature,
+		TopP:             cfg.DefaultParams.TopP,
+		MaxTokens:        cfg.DefaultParams.MaxTokens,
+		N:                cfg.DefaultParams.N,
+		StopWords:        cfg.DefaultParams.StopWords,
+		Stream:           false, // unsupported right now
+		FrequencyPenalty: cfg.DefaultParams.FrequencyPenalty,
+		PresencePenalty:  cfg.DefaultParams.PresencePenalty,
+		LogitBias:        cfg.DefaultParams.LogitBias,
+		User:             cfg.DefaultParams.User,
+		Seed:             cfg.DefaultParams.Seed,
+		Tools:            cfg.DefaultParams.Tools,
+		ToolChoice:       cfg.DefaultParams.ToolChoice,
+		ResponseFormat:   cfg.DefaultParams.ResponseFormat,
+	}
+}
+
+func NewChatMessagesFromUnifiedRequest(request *schemas.UnifiedChatRequest) []ChatMessage {
+	messages := make([]ChatMessage, 0, len(request.MessageHistory)+1)
+
+	// Add items from messageHistory first and the new chat message last
+	for _, message := range request.MessageHistory {
+		messages = append(messages, ChatMessage{Role: message.Role, Content: message.Content})
+	}
+
+	messages = append(messages, ChatMessage{Role: request.Message.Role, Content: request.Message.Content})
+
+	return messages
+}
+
+// Chat sends a chat request to the specified OpenAI model.
+func (c *Client) Chat(ctx context.Context, request *schemas.UnifiedChatRequest) (*schemas.UnifiedChatResponse, error) {
+	// Create a new chat request
+	chatRequest := c.createChatRequestSchema(request)
+
+	chatResponse, err := c.doChatRequest(ctx, chatRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(chatResponse.ProviderResponse.Message.Content) == 0 {
+		return nil, ErrEmptyResponse
+	}
+
+	return chatResponse, nil
+}
+
+func (c *Client) createChatRequestSchema(request *schemas.UnifiedChatRequest) *ChatRequest {
+	// TODO: consider using objectpool to optimize memory allocation
+	chatRequest := c.chatRequestTemplate // hoping to get a copy of the template
+	chatRequest.Messages = NewChatMessagesFromUnifiedRequest(request)
+
+	return chatRequest
+}
+
+func (c *Client) doChatRequest(ctx context.Context, payload *ChatRequest) (*schemas.UnifiedChatResponse, error) {
+	// Build request payload
+	rawPayload, err := json.Marshal(payload)
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshal openai chat request payload: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.chatURL, bytes.NewBuffer(rawPayload))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create openai chat request: %w", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+string(c.config.APIKey))
+	req.Header.Set("Content-Type", "application/json")
+
+	// TODO: this could leak information from messages which may not be a desired thing to have
+	c.telemetry.Logger.Debug(
+		"openai chat request",
+		zap.String("chat_url", c.chatURL),
+		zap.Any("payload", payload),
+	)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send openai chat request: %w", err)
+	}
+
+	defer resp.Body.Close() // TODO: handle this error
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			c.telemetry.Logger.Error("failed to read openai chat response", zap.Error(err))
+		}
+
+		// TODO: Handle failure conditions
+		// TODO: return errors
+		c.telemetry.Logger.Error(
+			"openai chat request failed",
+			zap.Int("status_code", resp.StatusCode),
+			zap.String("response", string(bodyBytes)),
+			zap.Any("headers", resp.Header),
+		)
+
+		return nil, errs.ErrProviderUnavailable
+	}
+
+	// Read the response body into a byte slice
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.telemetry.Logger.Error("failed to read openai chat response", zap.Error(err))
+		return nil, err
+	}
+
+	// Parse the response JSON
+	var responseJSON map[string]interface{}
+	
+	err = json.Unmarshal(bodyBytes, &responseJSON)
+	if err != nil {
+		c.telemetry.Logger.Error("failed to parse openai chat response", zap.Error(err))
+		return nil, err
+	}
+
+	// Parse response
+	var response schemas.UnifiedChatResponse
+
+	var responsePayload schemas.ProviderResponse
+
+	var tokenCount schemas.TokenCount
+
+	message := responseJSON["choices"].([]interface{})[0].(map[string]interface{})["message"].(map[string]interface{})
+	messageStruct := schemas.ChatMessage{
+		Role:    message["role"].(string),
+		Content: message["content"].(string),
+	}
+
+	tokenCount = schemas.TokenCount{
+		PromptTokens:   responseJSON["usage"].(map[string]interface{})["prompt_tokens"].(float64),
+		ResponseTokens: responseJSON["usage"].(map[string]interface{})["completion_tokens"].(float64),
+		TotalTokens:    responseJSON["usage"].(map[string]interface{})["total_tokens"].(float64),
+	}
+
+	responsePayload = schemas.ProviderResponse{
+		ResponseId: map[string]string{"system_fingerprint": responseJSON["system_fingerprint"].(string)},
+		Message:    messageStruct,
+		TokenCount: tokenCount,
+	}
+
+	response = schemas.UnifiedChatResponse{
+		ID:               responseJSON["id"].(string),
+		Created:          float64(time.Now().Unix()),
+		Provider:         "openai",
+		Router:           "chat",
+		Model:            responseJSON["model"].(string),
+		Cached:           false,
+		ProviderResponse: responsePayload,
+	}
+
+	return &response, nil
+}
