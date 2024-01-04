c167d1f5e9152e2a32bfb5b3d01eabefb6309869
Max <mkrueger190@gmail.com>
2024-01-03 20:03:57 -0700

    #58: test

--- pkg/providers/octoml/client_test.go
+++ pkg/providers/octoml/client_test.go
@@ -2,8 +2,13 @@ package octoml
 
 import (
 	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
-	"fmt"
 
 	"glide/pkg/api/schemas"
 
@@ -12,24 +17,49 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestOpenAIClient_ChatRequest(t *testing.T) {
-	
+func TestOctoMLClient_ChatRequest(t *testing.T) {
+	// OctoML Chat API: https://docs.octoai.cloud/docs/text-gen-api-docs
+	octoMLMock := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rawPayload, _ := io.ReadAll(r.Body)
+
+		var data interface{}
+		// Parse the JSON body
+		err := json.Unmarshal(rawPayload, &data)
+		if err != nil {
+			t.Errorf("error decoding payload (%q): %v", string(rawPayload), err)
+		}
+
+		chatResponse, err := os.ReadFile(filepath.Clean("./testdata/chat.success.json"))
+		if err != nil {
+			t.Errorf("error reading octoml chat mock response: %v", err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_, err = w.Write(chatResponse)
+
+		if err != nil {
+			t.Errorf("error on sending chat response: %v", err)
+		}
+	})
+
+	octoMLServer := httptest.NewServer(octoMLMock)
+	defer octoMLServer.Close()
 
 	ctx := context.Background()
 	cfg := DefaultConfig()
+	cfg.BaseURL = octoMLServer.URL
 
 	client, err := NewClient(cfg, telemetry.NewTelemetryMock())
 	require.NoError(t, err)
 
 	request := schemas.UnifiedChatRequest{Message: schemas.ChatMessage{
-		Role:    "user",
+		Role:    "human",
 		Content: "What's the biggest animal?",
 	}}
 
 	response, err := client.Chat(ctx, &request)
 	require.NoError(t, err)
 
-	fmt.Println(response)
-
-	//require.Equal(t, "chatcmpl-123", response.ID)
+	require.Equal(t, cfg.Model, response.Model)
+	require.Equal(t, "cmpl-8ea213aece0747aca6d0608b02b57196", response.ID)
 }
