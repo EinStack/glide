8abb389a2b7fb46d9fe8440654e109511520b47f
Roman Glushko <roman.glushko.m@gmail.com>
2024-01-08 23:43:58 +0200

    #43: Removed unneeded method

--- pkg/config/fields/secret.go
+++ pkg/config/fields/secret.go
@@ -1,9 +1,7 @@
 package fields
 
 import (
-	"crypto/md5"
 	"encoding"
-	"encoding/hex"
 )
 
 // Secret is a string that is marshaled in an opaque way, so we are not leaking sensitive information
@@ -16,11 +14,4 @@ var _ encoding.TextMarshaler = Secret("")
 // MarshalText marshals the secret as `[REDACTED]`.
 func (s Secret) MarshalText() ([]byte, error) {
 	return []byte(maskedSecret), nil
-}
-
-// Hash generates a digest of the secret to be used instead of the actual secret value
-func (s Secret) Hash() string {
-	hash := md5.Sum([]byte(s))
-
-	return hex.EncodeToString(hash[:])
 }
