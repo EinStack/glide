36b1fadd3aac937702307b39110914e041dd31b2
Roman Glushko <roman.glushko.m@gmail.com>
2024-01-14 16:35:04 +0200

    #46: Covered LL by tests

--- pkg/routers/routing/least_latency_test.go
+++ pkg/routers/routing/least_latency_test.go
@@ -3,6 +3,7 @@ package routing
 import (
 	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"glide/pkg/providers"
@@ -25,6 +26,76 @@ func TestLeastLatencyRouting_Warmup(t *testing.T) {
 		"all cold models & unhealthy": {[]Model{{"first", true, 0.0}, {"second", false, 0.0}, {"third", true, 0.0}}, []string{"first", "third", "first"}},
 		"some models are warmed":      {[]Model{{"first", true, 100.0}, {"second", true, 0.0}, {"third", true, 120.0}}, []string{"second", "second", "second"}},
 		"cold unhealthy model":        {[]Model{{"first", true, 120.0}, {"second", false, 0.0}, {"third", true, 100.0}}, []string{"third", "third", "third"}},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			models := make([]providers.Model, 0, len(tc.models))
+
+			for _, model := range tc.models {
+				models = append(models, providers.NewLangModelMock(model.modelID, model.healthy, model.latency))
+			}
+
+			routing := NewLeastLatencyRouting(models)
+			iterator := routing.Iterator()
+
+			// loop three times over the whole pool to check if we return back to the begging of the list
+			for _, modelID := range tc.expectedModelIDs {
+				model, err := iterator.Next()
+
+				require.NoError(t, err)
+				require.Equal(t, modelID, model.ID())
+			}
+		})
+	}
+}
+
+func TestLeastLatencyRouting_Routing(t *testing.T) {
+	type Model struct {
+		modelID  string
+		healthy  bool
+		latency  float64
+		expireAt time.Time
+	}
+
+	type TestCase struct {
+		models           []Model
+		expectedModelIDs []string
+	}
+
+	tests := map[string]TestCase{
+		"no cold expired models": {
+			[]Model{
+				{"first", true, 100.0, time.Now().Add(30 * time.Second)},
+				{"second", true, 80.0, time.Now().Add(30 * time.Second)},
+				{"third", true, 101.0, time.Now().Add(30 * time.Second)},
+			},
+			[]string{"second", "second", "second"},
+		},
+		"one expired model": {
+			[]Model{
+				{"first", true, 100.0, time.Now().Add(30 * time.Second)},
+				{"second", true, 80.0, time.Now().Add(30 * time.Second)},
+				{"third", true, 101.0, time.Now().Add(-time.Duration(30) * time.Second)},
+			},
+			[]string{"third", "second", "second"},
+		},
+		"two expired models": {
+			[]Model{
+				{"first", true, 100.0, time.Now().Add(-time.Duration(60) * time.Second)},
+				{"second", true, 80.0, time.Now().Add(30 * time.Second)},
+				{"third", true, 101.0, time.Now().Add(-time.Duration(30) * time.Second)},
+			},
+			[]string{"first", "third", "second"},
+		},
+		"all expired models": {
+			[]Model{
+				{"first", true, 100.0, time.Now().Add(-time.Duration(30) * time.Second)},
+				{"second", true, 80.0, time.Now().Add(-time.Duration(20) * time.Second)},
+				{"third", true, 101.0, time.Now().Add(-time.Duration(60) * time.Second)},
+			},
+			[]string{"third", "first", "second"},
+		},
 	}
 
 	for name, tc := range tests {
